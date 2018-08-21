f2fcea1a07ae4d7100e8c431f3d60fc36c97f1ce
Jed Peek <jedpeek@yahoo.com>
2018-08-21 01:39:01 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,7 +20,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:person).permit(
+    params.require(:recipe).permit(
     :title,
     ingredients_attributes:[
       :name,
